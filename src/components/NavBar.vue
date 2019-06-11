93a9dfdb7f91d0c0e41cc8cbf8bf756299087d41
Dedi Irawan <irawand07@gmail.com>
2019-06-11 16:22:33 +0700

    update layout

---
+++ src/components/NavBar.vue
@@ -0,0 +1,69 @@
+<template>
+  <nav class="navbar is-light">
+    <div class="navbar-brand">
+      <div class="navbar-item" href="/">
+        Vue multi layouts
+      </div>
+
+      <a class="navbar-burger" @click="open = !open">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div class="navbar-menu" :class="{'is-active': open}">
+      <div class="navbar-start">
+        <div class="navbar-item">
+          <router-link class="button is-primary" :to="{name: 'about'}" @click.native="open = false">
+            About
+          </router-link>
+        </div>
+        <div class="navbar-item">
+          <router-link class="button is-primary" to="/" @click.native="open = false">
+            Posts
+          </router-link>
+        </div>
+        <div class="navbar-item">
+          <router-link class="button is-primary" :to="{name: 'photos'}" @click.native="open = false">
+            Photos
+          </router-link>
+        </div>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <a href="https://itnext.io/@arieldi" @click="open = false">
+            @darkylmnx
+          </a>
+        </div>
+        <div class="navbar-item">
+          <a href="https://foreignrap.com/" @click="open = false">
+            Foreignrap
+          </a>
+        </div>
+        <div class="navbar-item">
+          <a href="http://mustsee.earth" @click="open = false">
+            Like traveling ? Click
+          </a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      open: false
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.navbar {
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.2);
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import Default from "./layouts/Default.vue";
+import NoSidebar from "./layouts/NoSidebar.vue";
+
+Vue.component("default-layout", Default);
+Vue.component("no-sidebar-layout", NoSidebar);
+
 Vue.config.productionTip = false;
 
 new Vue({
