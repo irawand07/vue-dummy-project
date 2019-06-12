7946e3894b806e92a5459b1e62569349f44eb6bb
Dedi Irawan <irawand07@gmail.com>
2019-06-12 15:50:13 +0700

    update vuex

--- src/store.js
+++ src/store.js
@@ -1,10 +1,16 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import Auth from './store/Auth'
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
-  actions: {}
+  plugins: [createPersistedState()],
+  actions: {},
+  modules: {
+    Auth,
+  }
 });
